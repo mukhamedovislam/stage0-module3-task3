4e4112037ee6728cefce37e73d56eb85c4afe56d
mukhamedovislam <35482455+mukhamedovislam@users.noreply.github.com>
2022-09-06 12:00:47 +0300

    Update ReassigningValues.java

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -8,12 +8,9 @@ public class ReassigningValues {
 	third = 100;
 	System.out.println(first + "\n" + second + "\n" + third);
 
-	int linkToFirst;
-	int linkToSecond;
-	int linkToThird;
-	linkToFirst = first;
-	linkToSecond = second;
-	linkToThird = third;
+	int linkToFirst = first;
+	int linkToSecond = second;
+	int linkToThird = third;
 
 	first = 15;
 	second = 6;
